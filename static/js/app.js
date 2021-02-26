160cb5814e9ac09988060be3dc3faa9e02152caa
TimmyKid <35666159+TimmyKid@users.noreply.github.com>
2021-02-26 19:44:37 +0300

    DOM App.js

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+// App.js includes flows, mouse action on hovers and general gsap animations==================
+//MOUSE ACTION
+const cursor = document.querySelector(".cursor");
+const cursor2 = document.querySelector(".cursor2")
+
+document.addEventListener("mousemove", e => {
+  cursor.setAttribute( "style", "left: "+(e.clientX - 2.5)+"px; top:"+(e.clientY - 2.5)+"px;");
+  cursor2.setAttribute( "style", "left: "+(e.clientX - 15)+"px; top:"+(e.clientY - 15)+"px;");
+});
