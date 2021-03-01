408a316c84270d7000b1e3eec1ba0ca786230fe1
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-03-01 12:37:26 +0300

    Not so perfect footer.

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,4 @@ document.addEventListener("mousemove", e => {
   cursor.setAttribute( "style", "left: "+(e.clientX - 2.5)+"px; top:"+(e.clientY - 2.5)+"px;");
   cursor2.setAttribute( "style", "left: "+(e.clientX - 15)+"px; top:"+(e.clientY - 15)+"px;");
 });
+// document.write(new Date().getFullYear());
