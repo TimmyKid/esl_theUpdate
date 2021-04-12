2206f3a738f1da3985441ea57f2823110202c325
hilltimmy <35666159+TimmyKid@users.noreply.github.com>
2021-04-12 17:59:05 +0300

    Gsap animation1 {introText}

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,3 @@
-// App.js includes flows, mouse action on hovers and general gsap animations==================
 //MOUSE ACTION
 const cursor = document.querySelector(".cursor");
 const cursor2 = document.querySelector(".cursor2");
@@ -7,7 +6,21 @@ document.addEventListener("mousemove", e => {
   cursor.setAttribute( "style", "left: "+(e.clientX - 2.5)+"px; top:"+(e.clientY - 2.5)+"px;");
   cursor2.setAttribute( "style", "left: "+(e.clientX - 15)+"px; top:"+(e.clientY - 15)+"px;");
 });
-document.ready(function() {
-  document.getElementById("#sound")[0].play();
-});
+// document.ready(function() {
+//   document.getElementById("#sound")[0].play();
+// });
 // document.write(new Date().getFullYear());
+const timeline = gsap.timeline({defaults: {opacity: .5, duration: 3, ease: "circ"}})
+timeline
+  .from("#preFace > h1", { x: "-25%"})
+  .from("#preFace .text-center h1", { x: "25%"}, "<")
+
+let word = ["Elevete Solutions.", "."]
+let blinkr = gsap.to(".blinkr", {opacity: 0, ease: "power2.inOut", repeat: -2, duration: 1})
+
+const masterTln = gsap.timeline()
+word.forEach(word => {
+  let stl = gsap.timeline()
+  stl.to(".introText", {duration: 2, text: word})
+  masterTln.add(stl)
+})
